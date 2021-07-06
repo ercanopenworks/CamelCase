066699087ff90c1bc73b4a368e103f3831eb9311
ercanerdogan@gmail.com <ercanerdogan@gmail.com>
2021-07-06 06:52:06 +0300

    fixed

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CamelCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            int result = camelCase(s);
+            Console.WriteLine(result);
+        }
+
+        private static int camelCase(string s)
+        {
+            List<string> wordList = new List<string>();
+            int wordCount = 0;
+
+            for (int i = 0; i < s.Length-1; i++)
+            {
+                if (char.IsUpper(s[i]))
+                {
+                    wordCount++;
+                }
+            }
+
+            return wordCount + 1;
+        }
+    }
+}
