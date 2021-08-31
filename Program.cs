31ccf7bf066514f700c2b2f11271ea43fc286578
ercanerdogan@gmail.com <ercanerdogan@gmail.com>
2021-08-31 06:58:31 +0300

    camelCase2 method has been added.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace CamelCase
 {
@@ -9,7 +10,7 @@ namespace CamelCase
         {
             string s = Console.ReadLine();
 
-            int result = camelCase(s);
+            int result = camelCase2(s);
             Console.WriteLine(result);
         }
 
@@ -27,6 +28,43 @@ namespace CamelCase
             }
 
             return wordCount + 1;
+        }
+
+
+        private static int camelCase2(string str)
+        {
+            StringBuilder sb = new StringBuilder();
+            //sb.Append(str[0]);
+
+            List<char> items = new List<char>();
+           
+
+            for (int i = 1; i < str.Length - 1; i++)
+            {
+                if (char.IsPunctuation(str[i]) || char.IsWhiteSpace(str[i]))
+                {
+
+                    items.Add(str[i]);        
+                }
+            }
+
+            char[] pattern = items.ToArray();
+            var temp = str.Split(pattern);
+            sb.Append(temp[0]);
+
+            for(int i = 1; i < temp.Length; i++)
+            {
+                string camelCaseItem = temp[i][0].ToString().ToUpper();
+                for(int j = 1; j < temp[i].Length; j++)
+                {
+                    camelCaseItem += temp[i][j].ToString().ToLower();
+                }
+
+                sb.Append(camelCaseItem);
+            }
+
+            var result = sb.ToString();
+            return 0;
         }
     }
 }
